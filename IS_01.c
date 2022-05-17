812795d353f7f545232af8dec54728fb0f6c1aa3
Prasad Sahadev Jagtap <78202609+Prasadjagtap2712@users.noreply.github.com>
2022-05-17 16:31:30 +0530

    Add files via upload

---
+++ IS_01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str[]="Hello World";
+    int i,len;
+    len = strlen(str);
+    for(i=0;i<len;i++){
+            printf("%c",str[i]&127);
+    }
+    printf("\n");
+    for(int i=0;i<len;i++){
+            printf("%c",str[i]^127);
+    }
+    printf("\n");
+    for(int i=0;i<len;i++){
+            printf("%c",str[i]|127);
+    }
+    printf("\n");
+}
